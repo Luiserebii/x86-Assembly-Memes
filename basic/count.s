74c15ac88f48637d6266a0b4c3d24465fe0ac41c
Luiserebii <luis@serebii.io>
2020-02-16 21:28:17 -0500

    Beginning to write a basic count program with the equivalent of a while loop

---
+++ basic/count.s
@@ -0,0 +1,21 @@
+# Count all of the elements in a certain pre-defined list
+
+.section .data
+
+nums:
+	.long 1, 2, 3, 4, 5, 0
+
+.section .text
+.globl _start
+
+_start: 
+
+	movl $0, %eax   # Set register for our count to 0
+	
+
+while_not_zero:
+	
+	cmpl
+
+end_while_not_zero:
+
