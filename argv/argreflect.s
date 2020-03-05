01f4dfdc3381b01f507c5ab145b38ec4d2c45298
Luiserebii <luis@serebii.io>
2020-03-05 17:37:27 -0500

    Sketching out code for simply printing the buffer, also seeing a strcpy may be needed

--- argv/argreflect.s
+++ argv/argreflect.s
@@ -6,6 +6,14 @@
 #
 #
 
+.section .bss
+	.equ BUFFER_SIZE 1000
+	.lcomm buffer, BUFFER_SIZE
+
+.section .data
+title:
+	.ascii "=============\nargreflect\n=============\n\n\0"
+
 .section .text
 .globl _start
 
@@ -13,10 +21,23 @@ _start:
 	# Capture current stack position into base pointer
 	movl %esp, %ebp
 
-	movl 4(%ebp), %eax
-	movl 8(%ebp), %ebx
-	movl 12(%ebp), %ecx
-	
+	.equ ARGC_NUM, 0
+	.equ ARGV_N, 4
+
+	# Write piece to buffer
+	pushl $title
+	pushl $buffer
+	call strcpy
+
+	# Print buffer
+	.equ WRITE_SYSCALL, 4
+	.equ STDOUT, 1
+	movl $WRITE_SYSCALL, %eax
+	movl $STDOUT, %ebx
+	movl $buffer, %ecx
+	movl $BUFFER_SIZE, %edx
+	int $0x80
+
 	# Exit
 	movl $1, %eax
 	int $0x80
